111ba1ef103f5a147aea1bbb17b693b1b8868ee8
Pournima Rajagopal <pourni.r@gmail.com>
2021-04-10 23:36:19 -0500

    2nd commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512__340.jpg';
+  image3 = 'https://i.giphy.com/media/13XW2MJE0XCoM0/giphy.gif';
 
   constructor() { }
 
